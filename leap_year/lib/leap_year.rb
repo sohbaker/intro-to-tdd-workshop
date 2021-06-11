d649dcbdf9bec9294ffba3196269f4f716b85144
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2021-06-11 13:14:43 +0100

    Years divisible by 4 but not 100 are leap years.

--- leap_year/lib/leap_year.rb
+++ leap_year/lib/leap_year.rb
@@ -1,4 +1,4 @@
 def leap_year(year)
-  return true if year % 4 == 0
+  return true if year % 4 == 0 && year % 100 != 0
   false
 end
--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe 'leap_year' do
   it "categorises 2009 as not a leap year" do
     expect(leap_year(2009)).to eq(false)
   end
+
+  it "categorises 1700 as not being a leap year" do
+    expect(leap_year(1700)).to eq(false)
+  end
 end
