03177acc493b01d07ba5c1a8c68f135958bcb46e
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2021-06-11 12:32:26 +0100

    Reduced the number of early returns as this can be a valuable trade-off

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -2,8 +2,8 @@ def fizzbuzz(number)
   output = number.to_s
 
   return 'fizzbuzz' if number % 5 == 0 && number % 3 == 0
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number % 5 == 0
+  output = 'fizz' if number % 3 == 0
+  output = 'buzz' if number % 5 == 0
 
   output
 end
