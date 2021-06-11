42e26e9729c4a70a4e457888fca19bda1f0bdd10
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2021-06-11 13:36:18 +0100

    Removed obsolete tests.

--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -1,20 +1,12 @@
 require 'leap_year'
 
 RSpec.describe 'leap_year' do
-  it "categorises 2008 as a leap year" do
-    expect(leap_year(2008)).to eq(true)
-  end
-
   it "categorises 2009 as not a leap year" do
     expect(leap_year(2009)).to eq(false)
   end
 
   it "categorises 1700 as not being a leap year" do
     expect(leap_year(1700)).to eq(false)
-  end
-
-  it "categorises 2000 as being a leap year" do
-    expect(leap_year(2000)).to eq(true)
   end
 
   it "categorises years that are divisible by 4 but not 100 as leap years" do
