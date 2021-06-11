e89ce37068ff728cb9332577d168e1126363a95c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2021-06-11 12:11:46 +0100

    We want to use output later on. Moving the assignment to the top makes it safe to do that.

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(number)
+  output = number.to_s
+
   return 'fizzbuzz' if number % 5 == 0 && number % 3 == 0
   return 'buzz' if number % 5 == 0
   return 'fizz' if number % 3 == 0
 
-  output = number.to_s
   output
 end
 
