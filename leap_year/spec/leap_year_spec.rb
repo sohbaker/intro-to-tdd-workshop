0167d01c592ae4c0eadc3b70abf0b53939410a43
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2021-06-11 13:39:27 +0100

    Made the test describe when a year is NOT a leap year.

--- leap_year/spec/leap_year_spec.rb
+++ leap_year/spec/leap_year_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe 'leap_year' do
     expect(leap_year(2009)).to eq(false)
   end
 
-  it "categorises 1700 as not being a leap year" do
-    expect(leap_year(1700)).to eq(false)
-  end
-
   it "categorises years that are divisible by 4 but not 100 as leap years" do
     expect(leap_year(2008)).to eq(true)
   end
 
   it "categorises years that are divisible by 400 as leap years" do
     expect(leap_year(2000)).to eq(true)
+  end
+
+  it "categorises years that are divisible by 100 but not 400 as not being leap years" do
+    expect(leap_year(1700)).to eq(false)
   end
 end
