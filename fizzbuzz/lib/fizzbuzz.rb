42f48fbb72cf22a1a6765b145eb020a8db44d780
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2021-06-11 12:37:50 +0100

    Moved variable closer to where it is used.

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
-  output = number.to_s
-
   return 'fizzbuzz' if number % 5 == 0 && number % 3 == 0
+
+  output = number.to_s
   output = 'fizz' if number % 3 == 0
   output = 'buzz' if number % 5 == 0
 
